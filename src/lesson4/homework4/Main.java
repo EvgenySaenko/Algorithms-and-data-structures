f9b7f8be3b95636e0224774420a8c1723c375e4c
Evgeny Saenko <storn84@mail.ru>
2020-07-07 13:44:02 +0300

    add homework4

---
+++ src/lesson4/homework4/Main.java
@@ -0,0 +1,29 @@
+package lesson4.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DoublyLinkedList<String> dl = new DoublyLinkedList<>();
+        dl.pushHead("data1");
+
+
+        System.out.println(dl.toString());
+        System.out.println(dl.getSize());
+
+
+        IteratorForDoublyLinkedList iter = dl.iterator();
+        iter.insertAfter("data2");
+        iter.insertAfter("data3");
+        iter.insertAfter("data4");
+
+        System.out.println(dl.toString());
+
+
+
+        //System.out.println("текущий элемент - "+ iter.next());
+//        System.out.println(iter.hasNext());
+//        System.out.println(iter.atEnd());
+
+
+    }
+}
